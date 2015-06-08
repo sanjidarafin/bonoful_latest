6ec63af45a283df0dc813cf563c6a698c79443a8
sanjida <sanjida1024@gmail.com>
2015-06-08 09:56:11 +0000

    admin/vieworder edit

--- admin/vieworder.php
+++ admin/vieworder.php
@@ -1,63 +1,63 @@
-<?php
-session_start();
-include_once("connection.php");
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>View shopping cart</title>
-<link href="style/style.css" rel="stylesheet" type="text/css"></head>
-<body>
-<div id="products-wrapper">
- <h1>View Order</h1>
- <div class="view-cart">
- 	<?php
-    $current_url = base64_encode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-	if(isset($_SESSION["products"]))
-    {
-	    $total = 0;
-		echo '<form method="post" action="viewordwe.php">';
-		echo '<ul>';
-		$cart_items = 0;
-		foreach ($_SESSION["products"] as $cart_itm)
-        {
-           $product_code = $cart_itm["code"];
-		   $results = $conn->query("SELECT * FROM order  LIMIT 1");
-		   $obj = $results->fetch_object();
-		   
-		    echo '<li class="cart-itm">';
-			echo '<span class="remove-itm"><a href="cart_update.php?removep='.$cart_itm["code"].'&return_url='.$current_url.'">&times;</a></span>';
-			echo '<div class="p-price">'.$currency.$obj->price.'</div>';
-            echo '<div class="product-info">';
-			echo '<h3>'.$obj->product_name.' (Code :'.$product_code.')</h3> ';
-            echo '<div class="p-qty">Qty : '.$cart_itm["qty"].'</div>';
-            echo '<div>'.$obj->product_desc.'</div>';
-			echo '</div>';
-            echo '</li>';
-			$subtotal = ($cart_itm["price"]*$cart_itm["qty"]);
-			$total = ($total + $subtotal);
 
-			echo '<input type="hidden" name="item_name['.$cart_items.']" value="'.$obj->product_name.'" />';
-			echo '<input type="hidden" name="item_code['.$cart_items.']" value="'.$product_code.'" />';
-			echo '<input type="hidden" name="item_desc['.$cart_items.']" value="'.$obj->product_desc.'" />';
-			echo '<input type="hidden" name="item_qty['.$cart_items.']" value="'.$cart_itm["qty"].'" />';
-			$cart_items ++;
-			
-        }
-    	echo '</ul>';
-		echo '<span class="check-out-txt">';
-		echo '<strong>Total : '.$currency.$total.'</strong>  ';
-		echo '</span>';
-    echo '<input type="submit">';
-		echo '</form>';
-		
-    }else{
-		echo 'Your Cart is empty';
-	}
-	
-    ?>
-    </div>
-</div>
-</body>
-</html>
+
+<?php require_once('connection.php'); ?>
+
+  
+<h1>View Orders </h1>
+<?php
+$sql = "SELECT * FROM customer, order WHERE customer.customer_id = order.customer_id";
+$result = $conn->query($sql);
+
+
+if ($result->num_rows > 0) {
+  
+  echo '<table border="1" style="width:100%">
+  <tr>
+    <td>ID</td>
+    <td>Customer Id</td>
+    <td>Order List</td>		
+    <td>Payment Method</td>		
+    <td>Shipment Address</td>
+    <td>Shipment Done</td>
+    <td>Date</td>
+    <td>Customer Id</td>
+    <td>Customer Id</td>
+  </tr>';
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      
+      
+  $id = $row["id"];    
+$customer_name = $row["customer_name"];
+ $customer_id= $row["customer_id"]; 
+$orderlist = $row["orderlist"];
+$payment_method = $row["payment_method"];
+$shipment_address = $row["shipment_address"];
+$shipment_done = $row["shipment_done"] ;
+$date = $row["date"];
+
+      
+      echo '
+  <tr>
+    <td>'.$id.'</td>
+    <td>'.$customer_name.'</td>		
+    <td>'.$customer_id.'</td>
+    <td>'.$orderlist.'</td>
+    <td>'.$payment_method.'</td>
+    <td>'.$shipment_address.'</td>	
+    <td>'.$shipment_done.'</td>		
+    <td>'.$date.'</td>
+  </tr>
+
+';
+
+    }
+  echo '</table>';
+  
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+  
+
--- paymentprocess.php
+++ paymentprocess.php
@@ -29,7 +29,7 @@ $customeremail = $_SESSION['user_email'];
     }
   }
   
-  $sql ="INSERT INTO `order` (`id`, `orderlist`, `customer_id`, `payment_method`, `shipment_address`, `shipment_done`, `date`) VALUES (NULL, '$orderlist', '$customerid', 'paypal', '$ship_address', '0', '2015-05-07 00:00:00');;";
+  $sql ="INSERT INTO `order` (`id`, `orderlist`, `customer_id`, `payment_method`, `shipment_address`, `shipment_done`, `date`) VALUES (NULL, '$orderlist', '$customerid', 'paypal', '$ship_address', '1', '2015-06-08 05:15:34');";
     if($conn->query($sql) == TRUE)
     {
     echo '<br><div style="padding: 5px; color:red">We have received your order. You will be notified Soon.</div></br>';
