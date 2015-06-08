af0f9823577eccdb48ec9af273ba473f1c91ff74
sanjida <sanjida1024@gmail.com>
2015-06-08 15:42:45 +0000

    admin/vieworder is added

--- admin/vieworder.php
+++ admin/vieworder.php
@@ -5,7 +5,7 @@
   
 <h1>View Orders </h1>
 <?php
-$sql = "SELECT * FROM customer, order WHERE customer.customer_id = order.customer_id";
+$sql = "SELECT * FROM customer, `order` WHERE customer.customer_id = order.customer_id";
 $result = $conn->query($sql);
 
 
@@ -14,14 +14,13 @@ if ($result->num_rows > 0) {
   echo '<table border="1" style="width:100%">
   <tr>
     <td>ID</td>
-    <td>Customer Id</td>
+    <td>Customer Name</td>
     <td>Order List</td>		
     <td>Payment Method</td>		
     <td>Shipment Address</td>
     <td>Shipment Done</td>
     <td>Date</td>
-    <td>Customer Id</td>
-    <td>Customer Id</td>
+   
   </tr>';
     // output data of each row
     while($row = $result->fetch_assoc()) {
@@ -29,7 +28,6 @@ if ($result->num_rows > 0) {
       
   $id = $row["id"];    
 $customer_name = $row["customer_name"];
- $customer_id= $row["customer_id"]; 
 $orderlist = $row["orderlist"];
 $payment_method = $row["payment_method"];
 $shipment_address = $row["shipment_address"];
@@ -41,7 +39,6 @@ $date = $row["date"];
   <tr>
     <td>'.$id.'</td>
     <td>'.$customer_name.'</td>		
-    <td>'.$customer_id.'</td>
     <td>'.$orderlist.'</td>
     <td>'.$payment_method.'</td>
     <td>'.$shipment_address.'</td>	
